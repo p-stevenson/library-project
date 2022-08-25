037bbc180c80acd24e5c2bfffbe7242d6b042442
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-08-25 14:14:22 +0100

    Add Book class and add book to library function

---
+++ script.js
@@ -0,0 +1,81 @@
+"use strict";
+let myLibrary = [
+  {
+    title: "Thinking, Fast and Slow",
+    author: "Daniel Kahneman",
+    pages: 295,
+    hasRead: true,
+  },
+  {
+    title: "Sapiens: A Brief History of Humankind",
+    author: "Yuval Noah Harari",
+    pages: 512,
+    hasRead: false,
+  },
+  { title: "1984", author: "George Orwell", pages: 250, hasRead: true },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    pages: 336,
+    hasRead: false,
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F.Scott Fitzgerald",
+    pages: 180,
+    hasRead: true,
+  },
+  {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    pages: 279,
+    hasRead: false,
+  },
+  { title: "Jane Eyre", author: "Charlotte Brontë", pages: 532, hasRead: true },
+  {
+    title: "The Picture of Dorian Grey",
+    author: "Oscar Wilde",
+    pages: 272,
+    hasRead: false,
+  },
+  {
+    title: "The Metamorphosis",
+    author: "Franz Kafka",
+    pages: 201,
+    hasRead: true,
+  },
+  {
+    title: "The Doors of Perception",
+    author: "Aldous Huxley",
+    pages: 208,
+    hasRead: false,
+  },
+  {
+    title: "Crime and Punishment",
+    author: "Fyodor Dostoyevsky",
+    pages: 671,
+    hasRead: true,
+  },
+];
+
+class Book {
+  constructor(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+  }
+}
+
+function addBookToLibrary(book) {
+  const title = prompt("title?");
+  const author = prompt("author");
+  const pages = prompt("Number of Pages?");
+  return myLibrary.push(
+    (book = new Book(`${title}`, `${author}`, `${pages}`, true))
+  );
+}
+
+function displayBooks() {}
+
+console.log("test");
