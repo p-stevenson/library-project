e2c5ee33f0b72a03c8edc3de3071f91615d34a13
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-09-02 14:58:10 +0100

    Add if statement to displayBooks()
    Don't append 'p' element if book property key === is 'uniqueID'
    Add if to check book property key === 'pages'
    Remove code to hide 'uuid' 'p' element when displayBooks() is called
    Update 'uuid' identifier to 'uniqueID'

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ let myLibrary = [
     title: "Crime and Punishment",
     author: "Fyodor Dostoyevsky",
     pages: 671,
-    uuid: crypto.randomUUID(),
+    uniqueID: crypto.randomUUID(),
   },
 ];
 
+class BookCard {
+  constructor() {}
+}
+
 class Book {
-  constructor(title, author, pages, uuid) {
+  constructor(title, author, pages, uniqueID) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.uuid = uuid;
+    this.uniqueID = uniqueID;
   }
 }
 
@@ -21,8 +25,8 @@ function addToLibrary(book) {
   const title = document.querySelector("#bookTitle").value;
   const author = document.querySelector("#author").value;
   const pages = Number(document.querySelector("#numOfPages").value);
-  const uuid = crypto.randomUUID();
-  return myLibrary.push((book = new Book(title, author, pages, uuid)));
+  const uniqueID = crypto.randomUUID();
+  return myLibrary.push((book = new Book(title, author, pages, uniqueID)));
 }
 
 function displayBooks(library) {
@@ -33,31 +37,38 @@ function displayBooks(library) {
     card.classList.add("card");
     card.setAttribute("id", `card0${i}`);
     cardContainer.appendChild(card);
-    const currentBook = document.querySelector(`[id=card0${i}]`);
 
+    const currentBook = document.querySelector(`[id=card0${i}]`);
     for (let key in library[i]) {
+      if (key === "uniqueID") {
+        continue;
+      }
       const info = document.createElement("p");
       info.classList.add("info");
       info.setAttribute("id", `${key}`);
       currentBook.appendChild(info);
-      info.textContent = library[i][key];
+      if (key === "pages") {
+        info.textContent = `Pages: ${library[i][key]}`;
+      } else {
+        info.textContent = library[i][key];
+      }
     }
 
     let delBtn = document.createElement("button");
     delBtn.classList.add("delBtn");
     delBtn.setAttribute("type", "button");
-    delBtn.setAttribute("id", `${myLibrary[i]["uuid"]}`);
+    delBtn.setAttribute("id", `${myLibrary[i]["uniqueID"]}`);
     delBtn.textContent = "Delete";
     currentBook.appendChild(delBtn);
 
-    const hideUUID = document.querySelectorAll("#uuid").forEach((item) => {
+    /*     const hideUUID = document.querySelectorAll("#uuid").forEach((item) => {
       item.style.display = "none";
-    });
+    }); */
   }
   const delBook = document.querySelectorAll(".delBtn").forEach((delBtn) =>
     delBtn.addEventListener("click", () => {
       const index = library.findIndex((book) => {
-        return book.uuid === `${delBtn["id"]}`;
+        return book.uniqueID === `${delBtn["id"]}`;
       });
       myLibrary.splice(index, 1);
       clearLibrary();
